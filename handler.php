af694455887d83d935a9e56a48124565fd09b803
scollet <origamillamaa@gmail.com>
2017-05-29 01:17:19 -0700

    newest code snippet. Will play around with tomorrow

--- handler.php
+++ handler.php
@@ -1,8 +1,79 @@
 <?php
-$target_dir = "testdata/"; /* Specify target directory */
-$target_file = $target_dir . basename($_FILES['userfile']['name']);
+
+header('Content-Type: text/plain; charset=utf-8');
+
+try {
+
+    // Undefined | Multiple Files | $_FILES Corruption Attack
+    // If this request falls under any of them, treat it invalid.
+    if (
+        !isset($_FILES['upfile']['error']) ||
+        is_array($_FILES['upfile']['error'])
+    ) {
+        throw new RuntimeException('Invalid parameters.');
+    }
+
+    // Check $_FILES['upfile']['error'] value.
+    switch ($_FILES['upfile']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
+    }
+
+    // You should also check filesize here.
+    if ($_FILES['upfile']['size'] > 1000000) {
+        throw new RuntimeException('Exceeded filesize limit.');
+    }
+
+    // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+    // Check MIME Type by yourself.
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    if (false === $ext = array_search(
+        $finfo->file($_FILES['upfile']['tmp_name']),
+        array(
+            'jpg' => 'image/jpeg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+        ),
+        true
+    )) {
+        throw new RuntimeException('Invalid file format.');
+    }
+
+    // You should name it uniquely.
+    // DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
+    // On this example, obtain safe unique name from its binary data.
+    if (!move_uploaded_file(
+        $_FILES['upfile']['tmp_name'],
+        sprintf('./uploads/%s.%s',
+            sha1_file($_FILES['upfile']['tmp_name']),
+            $ext
+        )
+    )) {
+        throw new RuntimeException('Failed to move uploaded file.');
+    }
+
+    echo 'File is uploaded successfully.';
+
+} catch (RuntimeException $e) {
+
+    echo $e->getMessage();
+
+}
+
+/*
+
+
+$target_dir = "testdata/"; // Specify target directory
+$target_file = $target_dir . basename($_FILES["data"]['name']);
 $uploadOk = 1;
-$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);*/
 // Check if image file is a actual image or fake image
 
 /*
@@ -21,13 +92,13 @@ if(isset($_POST["submit"])) {
 
 
 // Check if file already exists
-if (file_exists($target_file)) {
+/*if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES['userfile']['size'] > 500000) { /* Some number we get to set */
-    echo "Sorry, your file is too large.";
+if ($_FILES["data"]['size'] > 500000) { /* Some number we get to set */
+  /*  echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
@@ -49,9 +120,9 @@ if ($uploadOk == 0) {
       $name = basename($_FILES["uploadData"]["name"][$key]);
       move_uploaded_file($tmp_name, "data/$name");
   }
-}*/
-    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $target_file)) {
-        echo "The file ". basename( $_FILES['userfile']['name']). " has been uploaded.";
+}
+    if (move_uploaded_file($_FILES["data"]['tmp_name'], $target_file)) {
+        echo "The file ". basename( $_FILES["data"]['name']). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
