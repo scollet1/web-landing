0fe31f646321d97dfaea4339a4d14f25aa64b5ee
scollet <origamillamaa@gmail.com>
2017-05-29 00:37:34 -0700

    reorganzing php

--- handler.php
+++ handler.php
@@ -1,11 +1,13 @@
 <?php
 $target_dir = "testdata/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES['userfile']['name']);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
+
+/*
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES['userfile']['tmp_name']);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -14,20 +16,23 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
+*/
+
+
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES['userfile']['size'] > 500000) { /* Some number we get to set */
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($fileType != "jgrv") {
+    echo "Files must be uploaded with .jgrv extension";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -35,10 +40,30 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+/*
+  foreach ($_FILES['userfile']['error'] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+      $tmp_name = $_FILES['userfile']["tmp_name"][$key];
+      // basename() may prevent filesystem traversal attacks;
+      // further validation/sanitation of the filename may be appropriate
+      $name = basename($_FILES["uploadData"]["name"][$key]);
+      move_uploaded_file($tmp_name, "data/$name");
+  }
+}*/
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $target_file)) {
+        echo "The file ". basename( $_FILES['userfile']['name']). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+/*foreach ($_FILES["pictures"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+        // basename() may prevent filesystem traversal attacks;
+        // further validation/sanitation of the filename may be appropriate
+        $name = basename($_FILES["pictures"]["name"][$key]);
+        move_uploaded_file($tmp_name, "data/$name");
+    }
+}*/
+
 ?>
