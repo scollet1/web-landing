c7868d9fbd19238fe5c539c8302cc2bd3992a690
scollet <origamillamaa@gmail.com>
2017-05-30 02:04:09 -0700

    page is responsive now

--- js/upload.js
+++ js/upload.js
@@ -2,25 +2,29 @@ var form = document.getElementById('file-form');
 var fileSelect = document.getElementById('file-select');
 var uploadButton = document.getElementById('upload-button');
 
-form.onsubmit = submission_handler(event) {
+form.onsubmit = function(event) {
   event.preventDefault();
-
-  // Update button text.
+  console.log("Test1");
+  // Update button text using innerHTML;
   uploadButton.innerHTML = 'Uploading...';
 
-  // Get the selected files from the input.
+  // Get the selected files from the input;
   var files = fileSelect.files;
-  // Create a new FormData object.
+  // Create a new FormData object;
   var formData = new FormData();
 
   // Check the file type.
-  if (!file.type.match('*.jgrv')) {
+  file = files[0];
+  if (!file.type.match('\\*\\.jgrv')) {
     alert('Not a valid file type, please use the .jgrv extension');
-    continue ;
   }
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  else {
+    formData.append("file-select", file, file.name);
+  }
+  /* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   /**************************************************
-  // TODO : Validate file input for .jgrav extension;
+  ** TODO : Validate file input for .jgrav extension;
+  ** I don't know if this actually works;
   **************************************************/
 
   // Add the file to the request.
@@ -28,9 +32,9 @@ form.onsubmit = submission_handler(event) {
   // Set up the request.
   var xhr = new XMLHttpRequest();
   // Open the connection.
-  xhr.open('POST', 'handler.php', true); /* ARGUMENTS : HTTP Method=POST, URL Handler=handler.php, Asynchronous request?=true */
+  xhr.open('POST', "file:///nfs/2017/s/scollet/web-landing/handler.php", true); /* ARGUMENTS : HTTP Method=POST, URL Handler=handler.php, Asynchronous request?=true */
   // Set up a handler for when the request finishes.
-  xhr.onload = load_handler() {
+  xhr.onload = function() {
     if (xhr.status === 200) {
       // File(s) uploaded.
       uploadButton.innerHTML = 'Upload';
