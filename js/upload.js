0f9b9a6776b630766db858378c0840121e935994
scollet <origamillamaa@gmail.com>
2017-06-01 12:46:26 -0700

    yup

--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,5 @@
+Allow: POST
+
 var form = document.getElementById('file-form');
 var fileSelect = document.getElementById('file-select');
 var uploadButton = document.getElementById('upload-button');
