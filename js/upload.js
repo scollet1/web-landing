5617a9904661482a6e4912edaa9961f55300cbb2
scollet <origamillamaa@gmail.com>
2017-06-01 21:55:12 -0700

    requiring XHR

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,7 @@
 var form = document.getElementById('file-form');
 var fileSelect = document.getElementById('file-select');
 var uploadButton = document.getElementById('upload-button');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 form.onsubmit = function(event) {
   event.preventDefault();
